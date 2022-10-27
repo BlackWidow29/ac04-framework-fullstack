4f1529ce35f7292ad8660dfabc6deb3dae8e8819
rafaela <rafaela.augusto@somei.com.vc>
2022-10-26 23:29:12 -0300

    fix in routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask
+# from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from .controller.rotas import app
 
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile('config.py')
+# app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
 db = SQLAlchemy(app)
+
--- app/controller/rotas.py
+++ app/controller/rotas.py
@@ -1,5 +1,11 @@
-from app import app, db
+# from app import app, db
+from flask import Flask, make_response, jsonify, request
+from flask_cors import CORS
 
-@app.route('/api/contrato')
-def contratos():
-    pass
+app = Flask(__name__)
+# CORS(app)
+app.config['JSON_SORT_KEYS'] = False
+
+@app.route('/')
+def root():
+    return "<h1>Ac04 de Fullstack</h1>" 
