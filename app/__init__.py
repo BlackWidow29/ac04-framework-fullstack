692b41ee6ea0b72bd011cf52dc53ff3eb1b0e121
Gustavo Valle <gustavofdovalle@gmail.com>
2022-10-28 01:32:17 -0300

    Class Endereço e UF
    Criação das classes Endereço e UF, unificação da branch grab e vinicius.

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,15 @@ db = SQLAlchemy(app)
 
 api.add_resource(rotas.Contrato, '/contrato/<string:numero_contrato>/')
 api.add_resource(rotas.AllContratos, '/contrato/')
+
+api.add_resource(rotas.Cliente,'/cliente/<string:cliente_id>/')
+api.add_resource(rotas.AllClientes,"/cliente/")
+
+api.add_resource(rotas.Telefone,'/telefone/<string:telefone_id>/')
+api.add_resource(rotas.AllTelefone,"/telefone/")
+
+api.add_resource(rotas.UF,'/Uf/<string:uf_id>/')
+api.add_resource(rotas.AllUF,"/uf/")
+
+api.add_resource(rotas.Endereco,'/Endereco/<string:cliente_id>/<string:endereco_id>/')
+api.add_resource(rotas.AllEndereco,"/Endereco/")
--- app/controller/rotas.py
+++ app/controller/rotas.py
@@ -1,4 +1,5 @@
 from inspect import Attribute
+from multiprocessing import Barrier
 from urllib import response
 from flask import request, jsonify, Flask
 from flask_restful import Resource
@@ -44,7 +45,6 @@ class Contrato(Resource):
             }
         return response
 
-
 class AllContratos(Resource):
     def post(delf):
         data = request.json
@@ -64,6 +64,88 @@ class AllContratos(Resource):
             parcela.save()
 
         contrato.save()
+
+
+class Cliente(Resource):
+    def get(delf, cliente_id):
+        cliente = Tables.Cliente.query.filter_by(cliente_id=cliente_id).first()
+        try:
+            response = {
+                'id': cliente.id,
+                'nome': cliente.nome,
+                'sobrenome': cliente.sobrenome,
+                'agencia': cliente.agencia,
+                'conta': cliente.conta,
+                'email': cliente.email,
+            }
+        except AttributeError:
+            response = {
+                'status': 'error',
+                'msg': 'Client não encontrada'
+            }
+        return response
+
+    def put(delf, cliente_id):
+        cliente = Tables.Cliente.query.filter_by(
+            cliente_id=cliente_id).first()
+        data = request.json
+        if 'nome' in data:
+            cliente.cliente_id = data['nome']
+        if 'sobrenome' in data:
+            cliente.sobrenome = data['sobrenome']
+        if 'agencia' in data:
+            cliente.agencia = data['agencia']
+        if 'conta' in data:
+            cliente.conta = data['conta']
+        if 'email' in data:
+            cliente.email = data['email']
+
+        cliente.save()
+        response = {
+                'id': cliente.id,
+                'nome': cliente.nome,
+                'sobrenome': cliente.sobrenome,
+                'agencia': cliente.agencia,
+                'conta': cliente.conta,
+                'email': cliente.email,
+        }
+        return response
+
+
+    def delete(self, cliente_id):
+        try:
+            cliente = Tables.Cliente.query.filter_by(
+                cliente_id=cliente_id).first()
+            status = 'success'
+            msg = 'Cliente {} inativado com sucesso'.format(
+                cliente.cliente_id)
+            cliente.delete()
+        except AttributeError:
+            status = 'error'
+            msg = 'Cliente com o id {} não encontrado'.format(cliente_id)
+        return jsonify({'status': status, 'mensagem': msg})
+
+class AllClientes(Resource):
+    def get(delf):
+        clientes = Tables.Cliente.query.all()
+        response = [{'id': i.id, 'nome': i.nome, 'sobrenome': i.sobrenome,
+                     'agencia': i.agencia, 'conta': i.conta, 'email': i.email} for i in clientes]
+        return jsonify(response)
+
+    def post(delf):
+        data = request.json
+        cliente = Tables.Cliente(
+            nome=data['nome'], sobrenome=data['sobrenome'], agencia=data['agencia'], conta=data['conta'], email=data['email'])
+        cliente.save()
+        response = {
+                'id': cliente.id,
+                'nome': cliente.nome,
+                'sobrenome': cliente.sobrenome,
+                'agencia': cliente.agencia,
+                'conta': cliente.conta,
+                'email': cliente.email,
+        }
+        return response
 
 
 class Telefone(Resource):
@@ -95,3 +177,119 @@ class AllTelefone(Resource):
         telefone = Tables.Telefone(
             id=data['id'], numero=data['numero'], principal=data['principal'], id_cliente=data['id_cliente'])
         telefone.save()
+
+
+class UF(Resource):
+    def get(delf, id_uf):
+
+        id = Tables.Uf.query.filter_by(id=id).first()
+        uf = Tables.Uf.query.filter_by(id=id)
+        
+        try:
+            response = {
+                'id': id,
+                'uf': uf
+            }
+        except AttributeError:
+            response = {
+                'status': 'error',
+                'msg': 'UF não encontrado.'
+            }
+        return response
+        
+class AllUF(Resource):
+    def get(delf):
+        data = request.json
+        uf = Tables.Uf(
+            id=data['id'], uf=data['uf'])
+        uf.save() 
+        return response
+
+
+class Endereco(Resource):
+    def get(delf, cliente_id, endereco_id):
+        endereco = Tables.Endereco.query.filter_by(id = endereco_id).first()
+        cliente = Tables.Cliente.query.filter_by(
+            Tables.Cliente.id == cliente_id)
+        Uf = Tables.Uf.query.filter(
+            Tables.Uf.id == Uf)
+        try:
+            response = {
+                'id': endereco.id,
+                'cliente_id': cliente.id,
+                'cep': endereco.cep,
+                'logradouro': endereco.logradouro,
+                'bairro': endereco.bairro,
+                'numero': endereco.numero,
+                'cidade': endereco.cidade,
+                'complemento': endereco.complemento,
+                'uf': [
+                    {
+                        'id': Uf.id,
+                        'uf': Uf.uf
+                    }
+                ]
+            }
+        except AttributeError:
+            response = {
+                'status': 'error',
+                'msg': 'Endereço não encontrada'
+            }
+        return response
+
+    def put(delf, cliente_id, endereco_id):
+        cliente = Tables.Cliente.query.filter_by(
+            cliente_id=cliente_id).first()
+        endereco = Tables.Endereco.query.filter_by(
+            endereco_id = endereco_id).first()
+        data = request.json
+
+        if 'cep' in data:
+            endereco.cep = data['cep']
+        if 'logradouro' in data:
+            endereco.logradouro = data['logradouro']    
+        if 'bairro' in data:
+            endereco.bairro = data['bairro']
+        if 'numero' in data:
+            endereco.numero = data['numero']
+        if 'cidade' in data:
+            endereco.cidade = data['cidade']
+        if 'complemento' in data:
+            endereco.complemento = data['complemento']
+ 
+        endereco.save()
+        response = {
+                'id': endereco.id,
+                'cep': endereco.cep,
+                'logradouro': endereco.logradouro,
+                'bairro': endereco.bairro,
+                'numero': endereco.numero,
+                'cidade': endereco.cidade,
+                'complemento': endereco.complemento,
+        }
+        return response
+ 
+    def delete(self, endereco_id):
+        try:
+            endereco = Tables.Endereco.query.filter_by(
+                endereco_id = endereco_id).first()
+            status = 'success'
+            msg = 'Endereço {} removido com sucesso'.format(
+                endereco.id)
+            endereco.delete()
+        except AttributeError:
+            status = 'error'
+            msg = 'Endereço com o id {} não encontrado'.format(endereco)
+        return jsonify({'status': status, 'mensagem': msg})
+        
+class AllEndereco(Resource):
+    def get(delf):
+        data = request.json
+        endereco = Tables.Endereco(
+            id=data['id'], cep=data['cep'],
+            logradouro=data['logradouro'], bairro=data['bairro'],
+            numero=data['numero'], cidade=data['cidade'],
+            complmeneto=data['complmeneto']
+        )
+        endereco.save() 
+        return response
